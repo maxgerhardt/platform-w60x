d45faac57ac93aba4b8085cf86cdd0a6a12ccc6c
Maximilian Gerhardt <maxi.gerhardt@googlemail.com>
2020-06-01 16:43:23 +0200

    Fix naming of framework

--- builder/main.py
+++ builder/main.py
@@ -83,13 +83,6 @@ if not env.get("PIOFRAMEWORK"):
 # Target: Build executable and linkable firmware
 #
 
-if "zephyr" in env.get("PIOFRAMEWORK", []):
-    env.SConscript(
-        join(platform.get_package_dir(
-            "framework-zephyr"), "scripts", "platformio", "platformio-build-pre.py"),
-        exports={"env": env}
-    )
-
 target_elf = None
 if "nobuild" in COMMAND_LINE_TARGETS:
     target_elf = join("$BUILD_DIR", "${PROGNAME}.elf")
@@ -119,13 +112,7 @@ debug_tools = board.get("debug.tools", {})
 upload_source = target_firm
 upload_actions = []
 
-if upload_protocol == "mbed":
-    upload_actions = [
-        env.VerboseAction(env.AutodetectUploadPort, "Looking for upload disk..."),
-        env.VerboseAction(env.UploadToDisk, "Uploading $SOURCE")
-    ]
-
-elif upload_protocol.startswith("blackmagic"):
+if upload_protocol.startswith("blackmagic"):
     env.Replace(
         UPLOADER="$GDB",
         UPLOADERFLAGS=[
@@ -179,61 +166,6 @@ elif upload_protocol.startswith("jlink"):
     upload_actions = [env.VerboseAction("$UPLOADCMD", "Uploading $SOURCE")]
 
 
-elif upload_protocol == "dfu":
-    hwids = board.get("build.hwids", [["0x0483", "0xDF11"]])
-    vid = hwids[0][0]
-    pid = hwids[0][1]
-
-    # default tool for all boards with embedded DFU bootloader over USB
-    _upload_tool = "dfu-util"
-    _upload_flags = [
-        "-d", "vid:pid,%s:%s" % (vid, pid),
-        "-a", "0", "-s",
-        "%s:leave" % board.get("upload.offset_address", "0x08000000"), "-D"
-    ]
-
-    upload_actions = [env.VerboseAction("$UPLOADCMD", "Uploading $SOURCE")]
-
-    if board.get("build.mcu").startswith("stm32f103") and "arduino" in env.get(
-        "PIOFRAMEWORK"):
-        # F103 series doesn't have embedded DFU over USB
-        # stm32duino bootloader (v1, v2) is used instead
-        def __configure_upload_port(env):
-            return basename(env.subst("$UPLOAD_PORT"))
-
-        _upload_tool = "maple_upload"
-        _upload_flags = [
-            "${__configure_upload_port(__env__)}",
-            board.get("upload.boot_version", 2),
-            "%s:%s" % (vid[2:], pid[2:])
-        ]
-
-        env.Replace(__configure_upload_port=__configure_upload_port)
-
-        upload_actions.insert(
-            0, env.VerboseAction(env.AutodetectUploadPort,
-                                 "Looking for upload port..."))
-
-    if _upload_tool == "dfu-util":
-        # Add special DFU header to the binary image
-        env.AddPostAction(
-            join("$BUILD_DIR", "${PROGNAME}.bin"),
-            env.VerboseAction(
-                " ".join([
-                    '"%s"' % join(platform.get_package_dir("tool-dfuutil") or "",
-                         "bin", "dfu-suffix"),
-                    "-v %s" % vid,
-                    "-p %s" % pid,
-                    "-d 0xffff", "-a", "$TARGET"
-                ]), "Adding dfu suffix to ${PROGNAME}.bin"))
-
-    env.Replace(
-        UPLOADER=_upload_tool,
-        UPLOADERFLAGS=_upload_flags,
-        UPLOADCMD='$UPLOADER $UPLOADERFLAGS "${SOURCE.get_abspath()}"')
-
-    upload_source = target_firm
-
 elif upload_protocol == "serial":
     def __configure_upload_port(env):
         return env.subst("$UPLOAD_PORT")
@@ -250,20 +182,6 @@ elif upload_protocol == "serial":
         UPLOADCMD='$UPLOADER $UPLOADERFLAGS "$SOURCE" "${__configure_upload_port(__env__)}"'
     )
 
-    upload_actions = [
-        env.VerboseAction(env.AutodetectUploadPort, "Looking for upload port..."),
-        env.VerboseAction("$UPLOADCMD", "Uploading $SOURCE")
-    ]
-
-elif upload_protocol == "hid":
-    def __configure_upload_port(env):
-        return basename(env.subst("$UPLOAD_PORT"))
-
-    env.Replace(
-        __configure_upload_port=__configure_upload_port,
-        UPLOADER="hid-flash",
-        UPLOADCMD='$UPLOADER "$SOURCES" "${__configure_upload_port(__env__)}"'
-    )
     upload_actions = [
         env.VerboseAction(env.AutodetectUploadPort, "Looking for upload port..."),
         env.VerboseAction("$UPLOADCMD", "Uploading $SOURCE")
